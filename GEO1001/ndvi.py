d780779c74c40d53e16152643f7c2d19aa2112f9
Max van Schendel <maxvanschendel@gmail.com>
2019-10-17 20:29:03 +0200

    conv_filter_gemaakt

--- GEO1001/ndvi.py
+++ GEO1001/ndvi.py
@@ -3,6 +3,7 @@ from PIL import Image
 from matplotlib import pyplot as plt
 import time
 
+
 # load image to numpy array
 def load_image(fn):
     img = Image.open(fn)
@@ -12,15 +13,18 @@ def load_image(fn):
     return data
 
 
-def plot(ar):
-    plt.imshow(ar)
+# displays numpy array as image
+def plot(ar, cmap):
+    plt.imshow(ar, cmap)
     plt.show()
 
 
+# calculate normalized difference vegetation index (ndvi)
 def calculate_ndvi(red, nir):
     return (nir - red) / (nir + red)
 
 
+# checks if x is between a and b
 def is_between(x, a, b):
     if b >= x >= a:
         return True
@@ -28,6 +32,7 @@ def is_between(x, a, b):
         return False
 
 
+# finds class which a value belongs to based on ranges
 def classify(val):
     classes = {'built': [-1.0, -.1],
                'water': [-.1, 0.1],
@@ -40,26 +45,52 @@ def classify(val):
             return i[0]
 
 
+# convert continuous ndvi value to discrete classes
 def classify_image(red_image, nir_image):
     ndvi_vector = np.vectorize(calculate_ndvi)
     ndvi = ndvi_vector(red_image, nir_image)
-
     classify_vector = np.vectorize(classify)
+
     return classify_vector(ndvi)
 
 
-if __name__ == '__main__':
-    rgb_image = load_image('.\\satellite_images\\rgb.tif')
-    red = rgb_image[:, :, 0]
+# generate black and white images for each class
+def generate_masks(classified):
+    classes = {'built': [-1.0, -.1],
+               'water': [-.1, 0.1],
+               'low density vegetation': [0.1, 0.4],
+               'high density vegetation': [0.4, 1]}
 
+    classes_keys, classes_vals = list(classes.keys()), classes.values()
+
+    masks = {}
+    for i in range(len(classes_vals)):
+        masks[classes_keys[i]] = classified == i
+
+    return masks
+
+
+# generates land use masks from color  and near-infrared images
+# using the Normalized Difference Vegetation Index
+# global data can be obtained from SentinelHub
+if __name__ == '__main__':
+    # load images to numpy array
+    rgb_image = load_image('.\\satellite_images\\rgb.tif')
     infrared_image = load_image('.\\satellite_images\\nir.tif')
+
+    # extract red and nir channels from images
+    red = rgb_image[:, :, 0]
     nir = infrared_image[:, :, 0]
 
     start_time = time.time()
 
+    # classify land use and generate masks for each class
     classified_ndvi = classify_image(red, nir)
+    landuse_masks = generate_masks(classified_ndvi)
 
+    # evaluate performance
     run_time = time.time() - start_time
-    print('time: {}\npixels: {}\npps: {}'.format(run_time, red.size, int(red.size/run_time)))
+    print('time: {}\npixels: {}\npps: {}'.format(run_time, red.size, int(red.size / run_time)))
 
-    plot(classified_ndvi)
+    # display mask
+    plot(landuse_masks['built'], cmap='Greys')
