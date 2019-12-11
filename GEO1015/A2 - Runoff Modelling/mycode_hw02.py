5ec0d249cf83804434a7cfb61d1640c22c3abdba
maxvanschendel <maxvanschendel@gmail.com>
2019-12-11 19:30:55 +0100

    a

--- GEO1015/A2 - Runoff Modelling/mycode_hw02.py
+++ GEO1015/A2 - Runoff Modelling/mycode_hw02.py
@@ -7,6 +7,11 @@ import math
 import rasterio
 import numpy as np
 import matplotlib.pyplot as plt
+from heapq import *
+
+
+def get_neighbours(data, x, y):
+	return data[x-1:x+2, y-1:y+2]
 
 
 # gets all points that are outlets, these are either:
@@ -20,15 +25,40 @@ def get_outlets(elevation):
 
 	# get points that have a zero neighbours but are not zero themselves
 	for (x, y), item in np.ndenumerate(elevation):
-		if 0 in elevation[x-1:x+2, y-1:y+2] and item:
+		if 0 in get_neighbours(elevation, x, y) and item:
 			outlets[x, y] = 1
 
 	return outlets
 
 
-# gets index of the lower elevation in the terrain
-def lowest_elevation_index(elevation):
-	return np.unravel_index(np.argmin(elevation, axis=None), elevation.shape)
+def minimum_elevation(neighbours):
+	minimum = (math.inf, (None, None))
+	for (x, y), item in np.ndenumerate(neighbours):
+		if item < minimum[0] and item != 0.0 and (x, y) != (1, 1):
+			minimum = (item, (x, y))
+	return minimum
+
+
+# converts 2d numpy array to priority queue
+def np_to_pq(ar):
+	# construct priority queue
+	pq = []
+	heapify(pq)
+
+	for (x, y), item in np.ndenumerate(ar):
+		heappush(pq, (item, (x, y)))
+
+	return pq
+
+
+def least_steep_uphill_slope(elevation, x, y):
+	neighbours = get_neighbours(elevation, x, y)
+	minimum_neighbour = minimum_elevation(neighbours)
+
+	x_elev = x - minimum_neighbour[1][0] - 1
+	y_elev = y - minimum_neighbour[1][1] - 1
+
+	return elevation[x_elev][y_elev], (x_elev, y_elev)
 
 
 def flow_direction(elevation):
@@ -44,9 +74,25 @@ def flow_direction(elevation):
  
 	"""
 
+	flow_direction = np.zeros(elevation.shape)
+
+	# get outlets
 	outlets = get_outlets(elevation)
-	masked_terrain = np.ma.masked_array(elevation,outlets)
-	ind = lowest_elevation_index(masked_terrain)
+	masked_terrain = np.ma.masked_array(elevation, outlets)
+
+	# construct priority queue from outlets
+	priority_queue = np_to_pq(masked_terrain)
+
+	searching = True
+	while searching:
+		cur = heappop(priority_queue)
+
+		climbing = True
+		while climbing:
+			cur = least_steep_uphill_slope(elevation, cur[1][0], cur[1][1])
+
+		searching = False
+
 
 
 def flow_accumulation(directions):
