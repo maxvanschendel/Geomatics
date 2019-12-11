f11057014026476e4cfebf23cbb40d8c2eff2a3c
maxvanschendel <maxvanschendel@gmail.com>
2019-12-11 03:02:43 +0100

    a

--- GEO1015/A2 - Runoff Modelling/mycode_hw02.py
+++ GEO1015/A2 - Runoff Modelling/mycode_hw02.py
@@ -1,7 +1,7 @@
-#-- mycode_hw02.py
-#-- Assignment 02 GEO1015 (2019-2020)
-#-- [YOUR NAME] 
-#-- [YOUR STUDENT NUMBER]
+# -- mycode_hw02.py
+# -- Assignment 02 GEO1015 (2019-2020)
+# -- [YOUR NAME]
+# -- [YOUR STUDENT NUMBER]
 
 import math
 import rasterio
@@ -15,20 +15,15 @@ import matplotlib.pyplot as plt
 def get_outlets(elevation):
 	outlets = np.ones(elevation.shape)
 
-	# mask outlets on edge of array
-	outlets[1:elevation.shape[0] - 1, 1:elevation.shape[1] - 1] = 0
+	# get points on border of array
+	outlets[1:elevation.shape[0]-1, 1:elevation.shape[1]-1] = 0
 
-	# mask all points that have a zero neighbours but are not zero themselves
-	zero_elevation_neighbours = np.zeros(elevation.shape)
+	# get points that have a zero neighbours but are not zero themselves
 	for (x, y), item in np.ndenumerate(elevation):
 		if 0 in elevation[x-1:x+2, y-1:y+2] and item:
-			zero_elevation_neighbours[x, y] = 1
+			outlets[x, y] = 1
 
-	# combine zero value outlets with edge outlets
-	outlets += zero_elevation_neighbours
-	outlets = np.array(outlets, dtype=bool)
-
-	return np.ma.masked_array(elevation,outlets)
+	return outlets
 
 
 # gets index of the lower elevation in the terrain
@@ -38,63 +33,57 @@ def lowest_elevation_index(elevation):
 
 def flow_direction(elevation):
 	"""
-    !!! TO BE COMPLETED !!!
-     
-    Function that computes the flow direction
-     
-    Input:
-        elevation: grid with height values
-    Output:
-        returns grid with flow directions (encoded in whichever way you decide)
+	!!! TO BE COMPLETED !!!
+
+	Function that computes the flow direction
+
+	Input:
+		elevation: grid with height values
+	Output:
+		returns grid with flow directions (encoded in whichever way you decide)
  
-    """
+	"""
 
 	outlets = get_outlets(elevation)
-	ind = lowest_elevation_index(outlets)
-	print(ind)
-
-
-
-	# plt.imshow(outlets,cmap='gray')
-	# plt.show()
-	#
-	# plt.imshow(elevation)
-	# plt.show()
+	masked_terrain = np.ma.masked_array(elevation,outlets)
+	ind = lowest_elevation_index(masked_terrain)
 
 
 def flow_accumulation(directions):
 	"""
-    !!! TO BE COMPLETED !!!
-     
-    Function that computes the flow accumulation
-     
-    Input:
-        directions: grid with flow directions (encoded in whichever way you decide)
-    Output:
-        returns grid with accumulated flow (in number of upstream cells)
+	!!! TO BE COMPLETED !!!
+
+	Function that computes the flow accumulation
+
+	Input:
+		directions: grid with flow directions (encoded in whichever way you decide)
+	Output:
+		returns grid with accumulated flow (in number of upstream cells)
  
-    """  
+	"""
+
 
 def write_directions_raster(raster, input_profile):
 	"""
-    !!! TO BE COMPLETED !!!
-     
-    Function that writes the output flow direction raster
-     
-    Input:
-        raster: grid with flow directions (encoded in whichever way you decide)
-        input_profile: profile of elevation grid (which you can copy and modify)
+	!!! TO BE COMPLETED !!!
+
+	Function that writes the output flow direction raster
+
+	Input:
+		raster: grid with flow directions (encoded in whichever way you decide)
+		input_profile: profile of elevation grid (which you can copy and modify)
  
-    """  
+	"""
+
 
 def write_accumulation_raster(raster, input_profile):
 	"""
-    !!! TO BE COMPLETED !!!
-     
-    Function that writes the output flow accumulation raster
-     
-    Input:
-        raster: grid with accumulated flow (in number of upstream cells)
-        input_profile: profile of elevation grid (which you can copy and modify)
+	!!! TO BE COMPLETED !!!
+
+	Function that writes the output flow accumulation raster
+
+	Input:
+		raster: grid with accumulated flow (in number of upstream cells)
+		input_profile: profile of elevation grid (which you can copy and modify)
  
-    """  
+	"""
