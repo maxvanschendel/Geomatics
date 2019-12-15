2e5f3445475bdd2b70b950a037d4762cbbfce112
maxvanschendel <maxvanschendel@gmail.com>
2019-12-15 19:45:25 +0100

    a

--- GEO1003/A5/main.py
+++ GEO1003/A5/main.py
@@ -9,19 +9,27 @@ from sklearn import svm
 import numpy as np
 import os
 
-
+print('Loading data')
 merged_rooms = pd.DataFrame(columns=['datetime', 'mac', 'empty', 'ssid', 'rssi', 'channel', 'empty2', 'freq', 'amount', 'chipset',"room"])
 for i in os.listdir('./data'):
+    print('-',i)
     df = pd.read_csv('./data/'+i, names=['datetime', 'mac', 'empty', 'ssid', 'rssi', 'channel', 'empty2', 'freq', 'amount', 'chipset'], delimiter="\t")
     df['room'] = i[:-4]
     merged_rooms = merged_rooms.append(df)
 
 # preprocess data
+print('Preprocessing data')
+print('- Filtering eduroam')
 filtered_dataset = merged_rooms[merged_rooms['ssid'] == 'eduroam']
+
+print('- Shuffling')
 shuffled_dataset = filtered_dataset.sample(frac=1).reset_index(drop=True)
+
+print('- Grouping by datetime')
 grouped_by_datetime = group_dataset_by_datetime(shuffled_dataset)
 grouped_dataframe = pd.DataFrame(grouped_by_datetime).transpose()
 
+print('- Splitting train/test')
 # split data into training and testing data
 msk = np.random.rand(len(grouped_dataframe)) < 0.7
 train = grouped_dataframe[msk]
@@ -29,8 +37,8 @@ test = grouped_dataframe[~msk]
 
 X_train, X_test = train['data'], test['data']
 Y_train, Y_test = train['room'], test['room']
-print(Y_train.unique())
 
+print('- Converting dicts to dataframe')
 unique_macs = filtered_dataset['mac'].unique()
 X_columnized_train = pd.DataFrame(columns=unique_macs, dtype=int)
 X_columnized_test = pd.DataFrame(columns=unique_macs, dtype=int)
@@ -44,18 +52,29 @@ for i in enumerate(X_test.tolist()):
     for j in i[1]:
         X_columnized_test.loc[i[0], j['mac']] = j['rssi']
 
+print('- Setting NaN to zero')
 # set rssi of all mac addresses that haven't been registered to zero
 X_columnized_train = X_columnized_train.fillna(0)
 X_columnized_test = X_columnized_test.fillna(0)
 
+print('Fitting models')
 # construct and train neural network, random forest, support vector machine and logistic regression
-NN = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(32, 5), random_state=1,verbose=1).fit(X_columnized_train, Y_train)
-RF = RandomForestClassifier(n_estimators=100, max_depth=50, random_state=0,verbose=1).fit(X_columnized_train, Y_train)
-SVM = svm.SVC(decision_function_shape="ovo",verbose=1).fit(X_columnized_train, Y_train)
-LR = LogisticRegression(random_state=0, solver='lbfgs', multi_class='ovr',verbose=1).fit(X_columnized_train, Y_train)
+print('- Neural Network')
+NN = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(32, 5), random_state=1, max_iter=2500).fit(X_columnized_train, Y_train)
+
+print('- Random Forest')
+RF = RandomForestClassifier(n_estimators=100, max_depth=50, random_state=0).fit(X_columnized_train, Y_train)
+
+print('- Support Vector Machine')
+SVM = svm.SVC(decision_function_shape="ovo", max_iter=2500).fit(X_columnized_train, Y_train)
+
+print('- Logistic Regession')
+LR = LogisticRegression(random_state=0, solver='lbfgs', multi_class='ovr', max_iter=2500).fit(X_columnized_train, Y_train)
 
 # test prediction accuracy on test dataset
-print('NN score:', round(NN.score(X_columnized_test, Y_test), 4))
-print('RF score:', round(RF.score(X_columnized_test, Y_test), 4))
-print('SVM score:', round(SVM.score(X_columnized_test, Y_test), 4))
-print('LR score:', round(LR.score(X_columnized_test, Y_test), 4))
+print('Testing accuracy')
+print('- No. classes:', len(Y_train.unique()))
+print('- NN score:', round(NN.score(X_columnized_test, Y_test), 4))
+print('- RF score:', round(RF.score(X_columnized_test, Y_test), 4))
+print('- SVM score:', round(SVM.score(X_columnized_test, Y_test), 4))
+print('- LR score:', round(LR.score(X_columnized_test, Y_test), 4))
