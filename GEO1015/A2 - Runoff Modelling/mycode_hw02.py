1aced34b2f87d0da3cf3411c55704a76cb9e2adf
maxvanschendel <maxvanschendel@gmail.com>
2019-12-15 16:05:57 +0100

    a

--- GEO1015/A2 - Runoff Modelling/mycode_hw02.py
+++ GEO1015/A2 - Runoff Modelling/mycode_hw02.py
@@ -1,16 +1,19 @@
 # -- mycode_hw02.py
 # -- Assignment 02 GEO1015 (2019-2020)
-# -- [YOUR NAME]
-# -- [YOUR STUDENT NUMBER]
+# -- Max van Schendel
+# -- 4384644
 
 import math
 import rasterio
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
 import time
 from heapq import *
+import random
 
 
+# get neighbouring pixels by slicing np array
 def get_neighbours(data, x, y):
     return data[x - 1:x + 2, y - 1:y + 2]
 
@@ -32,6 +35,7 @@ def get_outlets(elevation):
     return outlets
 
 
+# get local index and value of neighbour with lowest elevation
 def minimum_elevation(neighbours):
     if neighbours.size:
         ind = np.unravel_index(np.argmin(neighbours, axis=None), neighbours.shape)
@@ -44,7 +48,6 @@ def minimum_elevation(neighbours):
 
 # converts 2d numpy array to priority queue
 def np_to_pq(ar):
-    # construct priority queue
     pq = []
     heapify(pq)
 
@@ -55,92 +58,134 @@ def np_to_pq(ar):
     return pq
 
 
+# map indices in sliced array to larger array; used for getting values of neighbour elements
 def local_to_global_coords(local_coords, global_center):
     return global_center[0] + local_coords[0] - 1, global_center[1] + local_coords[1] - 1
 
 
-def least_steep_uphill_slope(elevation, x, y):
+def minimum_neighbour(elevation, x, y):
     neighbours = get_neighbours(elevation, x, y)
-    minimum_neighbour = minimum_elevation(neighbours)
+    min_neighbour = minimum_elevation(neighbours)
 
-    if minimum_neighbour is not None:
-        global_coords = local_to_global_coords(minimum_neighbour[1], (x, y))
+    if min_neighbour is not None:
+        global_coords = local_to_global_coords(min_neighbour[1], (x, y))
         return elevation[global_coords[0]][global_coords[1]], global_coords
 
     else:
         return None
 
 
+def find_upstream_pixels(flow_directions, coords):
+    flow_direction_decode = {(0, 0): 1, (1, 0): 2, (2, 0): 3,
+                             (0, 1): 4, (1, 1): 5, (2, 1): 6,
+                             (0, 2): 7, (1, 2): 8, (2, 2): 9}
+
+    upstream_pixels = []
+
+    neighbours = get_neighbours(flow_directions, coords[0], coords[1])
+
+    for index in flow_direction_decode:
+        try:
+            val_at_index = neighbours[index[0]][index[1]]
+        except IndexError:
+            val_at_index = None
+
+        if flow_direction_decode[index] == val_at_index:
+            upstream_pixels.append(index)
+
+    random.shuffle(upstream_pixels)
+    return upstream_pixels
+
+
 def flow_direction(elevation):
     """
-	!!! TO BE COMPLETED !!!
-
 	Function that computes the flow direction
 
 	Input:
 		elevation: grid with height values
 	Output:
 		returns grid with flow directions (encoded in whichever way you decide)
- 
 	"""
 
-    time_start = time.clock()
-
     # flow direction array which the results will be written to
-    flow_direction = np.zeros(elevation.shape)
+    flow_dir = np.zeros(elevation.shape)
 
-    # get outlets
+    # get outlets and construct priority queue
     outlets = get_outlets(elevation)
-    masked_terrain = np.ma.masked_array(elevation, outlets)
+    priority_queue = np_to_pq(np.ma.masked_array(elevation, outlets))
 
-    # construct priority queue from outlets
-    priority_queue = np_to_pq(masked_terrain)
-
-    # initial pop
+    # get the current pixel in the array, in this case the first outlet in the priority queue
     cur = heappop(priority_queue)
 
     # keep searching until priority queue is empty
     while priority_queue:
 
-        i = 0
+        i = 1
         for coords, item in np.ndenumerate(get_neighbours(elevation, cur[1][0], cur[1][1])):
-            i += 1
             coords = local_to_global_coords(coords, cur[1])
 
             # check if item has already been calculated and if it not in the pq yet
-            if flow_direction[coords[0]][coords[1]] == 0 and (item, coords) not in priority_queue and (item, coords):
+            if flow_dir[coords[0]][coords[1]] == 0 and (item, coords) not in priority_queue and (item, coords):
                 heappush(priority_queue, (item, coords))
-                flow_direction[coords[0]][coords[1]] = i
+                flow_dir[coords[0]][coords[1]] = i
 
-        lsuhs = least_steep_uphill_slope(elevation, cur[1][0], cur[1][1])
+            i += 1
 
-        if lsuhs is None or flow_direction[lsuhs[1][0]][lsuhs[1][1]] != 0:
+        # neighbour with lowest elevation value
+        min_neighbour = minimum_neighbour(elevation, cur[1][0], cur[1][1])
+
+        # check if lowest neighbour is invalid or if its flow direction has already been set
+        if min_neighbour is None or flow_dir[min_neighbour[1][0]][min_neighbour[1][1]] != 0:
             cur = heappop(priority_queue)
         else:
-            cur = lsuhs
+            cur = min_neighbour
 
-    plt.imshow(flow_direction)
-    plt.show()
-    print(time_start - time.clock())
-    return flow_direction
+    plt.imshow(flow_dir, cmap='Set2')
+    plt.savefig('flow_dir.png')
+
+    return flow_dir
 
 
 def flow_accumulation(directions):
     """
-	!!! TO BE COMPLETED !!!
-
 	Function that computes the flow accumulation
 
 	Input:
 		directions: grid with flow directions (encoded in whichever way you decide)
 	Output:
 		returns grid with accumulated flow (in number of upstream cells)
- 
 	"""
 
-    flow_direction_decode = {1: (0, 0), 2: (1, 0), 3: (2, 0), 4: (0, 1),
-                             5: (1, 1), 6: (2, 1), 7: (0, 2), 8: (1, 2), 9: (2, 2)}
+    accumulated_flow = np.empty(directions.shape)
 
+    # for every pixel, count number of pixels that are uphill from it
+    for coords, item in np.ndenumerate(directions):
+        # initialize search queue
+        search_queue = [(0, coords)]
+        heapify(search_queue)
+
+        # processed pixels are marked to prevent walking in circles
+        processed = []
+
+        c = 0
+        while search_queue:
+            cur = heappop(search_queue)
+            upstream_pixels = find_upstream_pixels(directions, cur[1])
+
+            for i in upstream_pixels:
+                global_coords = local_to_global_coords(i, cur[1])
+
+                if global_coords not in processed:
+                    heappush(search_queue, (c, global_coords))
+                    processed.append(global_coords)
+
+                    accumulated_flow[coords[0]][coords[1]] += 1
+                    c += 1
+
+    plt.imshow(accumulated_flow, cmap='gray')
+    plt.savefig('acc_flow.png')
+
+    return accumulated_flow
 
 
 def write_directions_raster(raster, input_profile):
