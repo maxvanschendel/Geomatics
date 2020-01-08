e4ddd1534dfb8c818a643eb61367831d41fafe87
maxvanschendel <maxvanschendel@gmail.com>
2020-01-08 23:37:52 +0100

    a

--- GEO1015/A3 - Ground Filtering/my_code_hw03.py
+++ GEO1015/A3 - Ground Filtering/my_code_hw03.py
@@ -17,45 +17,40 @@ from scipy.spatial import cKDTree
 
 
 def point_cloud_to_grid(point_cloud, cell_size):
-    discarded_points = set()
-    ground_points = set()
+    ground_points, discarded_points = set(), set()
 
     X, Y, Z = point_cloud.X, point_cloud.Y, point_cloud.Z
     X_min, X_max, Y_min, Y_max = X.min(), X.max(), Y.min(), Y.max()
 
     stacked_array = np.vstack((X, Y, Z)).transpose()
-    rect_array = np.zeros((X_max + 1, Y_max + 1))
+    rect_array = np.zeros((X_max+1, Y_max+1))
 
     # converts the 1D array to a 2D X, Y array with Z elements
     for x, y, z in stacked_array:
-        if rect_array[x][y] == 0:
-            ground_points.add((x, y, z))
+        if rect_array[x][y] == 0 or z < rect_array[x][y]:
             rect_array[x][y] = z
-
-        elif rect_array[x][y] > z:
-            discarded_points.add((x, y, rect_array[x][y]))
-            ground_points.add((x, y, z))
-            rect_array[x][y] = z
-
-        else:
-            discarded_points.add((x, y, z))
 
     # create initial grid made of empty elements
-    gf_initgrid_shape = (int((X_max - X_min) / cell_size), int((Y_max - Y_min) / cell_size))
-    grid = np.empty(gf_initgrid_shape)
+    gf_initgrid_shape = (int((X_max-X_min)/cell_size), int((Y_max-Y_min)/cell_size))
 
-    # construct initial grid by slicing numpy array in chunks with cell size
+    # construct initial grid by slicing numpy array in chunks
     for x in range(gf_initgrid_shape[0]):
         for y in range(gf_initgrid_shape[1]):
-            x_range, y_range = (x * cell_size, (x + 1) * cell_size), (y * cell_size, (y + 1) * cell_size)
+            x_range, y_range = (x*cell_size, (x+1)*cell_size), (y*cell_size, (y+1)*cell_size)
+            cell_points = rect_array[x_range[0]: x_range[1], y_range[0]: y_range[1]]
 
-            points_in_cell = rect_array[x_range[0]:x_range[1], y_range[0]: y_range[1]]
-            pic_nozero = points_in_cell[points_in_cell != 0]
+            nonzero_indices = np.nonzero(cell_points)
+            nonzero_elements = cell_points[nonzero_indices]
 
-            if pic_nozero.size:
-                grid[x][y] = pic_nozero.min()
+            try:
+                min_index = np.argmin(nonzero_elements)
+                ground_points.add((nonzero_indices[0][min_index]+x_range[0],
+                                   nonzero_indices[1][min_index]+y_range[0],
+                                   nonzero_elements[min_index]))
+            except ValueError:
+                pass
 
-    return grid, discarded_points, ground_points
+    return ground_points, discarded_points
 
 
 def filter_ground(jparams):
@@ -84,12 +79,9 @@ def filter_ground(jparams):
     point_cloud = File(jparams['input-las'], mode='r')
 
     gridded_pc = point_cloud_to_grid(point_cloud, jparams['gf-cellsize'])
-    init_grid, unprocessed_points, ground_points = gridded_pc[0], gridded_pc[1], gridded_pc[2]
+    init_grid, unprocessed_points = gridded_pc[0], gridded_pc[1]
 
     init_delaunay = startin.DT(init_grid)
-
-
-
-
+    print(len(init_grid))
 
     return
