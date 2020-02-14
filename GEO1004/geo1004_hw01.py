cae2db9c94493634ad467552e86472d37ed2887f
maxvanschendel <maxvanschendel@gmail.com>
2020-02-14 11:56:35 +0100

    created structure

---
+++ GEO1004/geo1004_hw01.py
@@ -0,0 +1,23 @@
+#-- GEO1004.2020--hw01
+#-- Max van Schendel
+#-- 4384644
+
+
+def parse_obj(fn):
+    pass
+
+
+def construct_data_structure():
+    pass
+
+
+def group_triangles():
+    pass
+
+
+def align_normals():
+    pass
+
+
+def write_obj():
+    pass
