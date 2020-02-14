11ba85307b589b39448e2896346cf5d3b447a0da
maxvanschendel <maxvanschendel@gmail.com>
2020-02-14 15:53:39 +0100

    a

--- GEO1004/geo1004_hw01.py
+++ GEO1004/geo1004_hw01.py
@@ -1,18 +1,77 @@
-#-- GEO1004.2020--hw01
-#-- Max van Schendel
-#-- 4384644
+# -- GEO1004.2020--hw01
+# -- Max van Schendel
+# -- 4384644
+
+import numpy as np
+
+# CONSTRUCT DATA STRUCTURE BY GROWING FACE
+
+
+class Vertex:
+    def __init__(self, coords):
+        self.x = coords[0]
+        self.y = coords[1]
+        self.z = coords[2]
+
+
+class Face:
+    def __init__(self, vxs):
+        self.a, self.b, self.c = vxs[0], vxs[1], vxs[2]
+        self.vxs = set(vxs)
+        self.nb = []
+        self.normal = []
+
+    def find_neighbours(self, fs):
+        return filter(lambda x: len(self.vxs.intersection(x.vxs)) == 2, fs)
+
+
+class Mesh:
+    def __init__(self):
+        self.faces = set()
+
+    def get_faces(self):
+        return self.faces
+
+    def count_faces(self):
+        return len(self.faces)
 
 
 def parse_obj(fn):
-    pass
+    with open(fn) as obj_file:
+        lines = [i.replace('\n', '').split(' ') for i in obj_file.readlines()]
+
+    vxs, fs = [], []
+
+    for i in lines:
+        if i[0] == 'v':
+            vxs.append([float(i) for i in i[1:]])
+        elif i[0] == 'f':
+            fs.append([int(i) - 1 for i in i[1:]])
+
+    return vxs, fs
 
 
-def construct_data_structure():
-    pass
+def group_triangles(fs):
+    meshes = set()
 
+    while fs:
+        m = Mesh()
+        search_queue = set()
+        search_queue.add(fs[0])
 
-def group_triangles():
-    pass
+        while search_queue:
+            face = search_queue.pop()
+            face.nbs = face.find_neighbours(fs)
+
+            for n in face.nbs:
+                if n not in search_queue and n not in m.faces:
+                    m.faces.add(n)
+                    search_queue.add(n)
+                    fs.remove(n)
+
+        meshes.add(m)
+
+    return meshes
 
 
 def align_normals():
@@ -21,3 +80,12 @@ def align_normals():
 
 def write_obj():
     pass
+
+
+if __name__ == '__main__':
+    raw_geometry = parse_obj('./obj/bk_soup.obj')
+
+    # reconstruct geometry from polygon soup
+    vertices = [Vertex(i) for i in raw_geometry[0]]
+    faces = [Face((vertices[i[0]], vertices[i[1]], vertices[i[2]])) for i in raw_geometry[1]]
+    meshes = group_triangles(faces)
