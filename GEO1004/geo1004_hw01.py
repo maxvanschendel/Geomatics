c2e05cc33240ecaec622800a3e8a020b1848ad25
maxvanschendel <maxvanschendel@gmail.com>
2020-02-17 16:58:57 +0100

    a

--- GEO1004/geo1004_hw01.py
+++ GEO1004/geo1004_hw01.py
@@ -5,6 +5,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import math
 
 
 class Vertex:
@@ -19,6 +20,7 @@ class Face:
         self.vxs = set(vxs)
         self.nbs = set()
         self.processed = False
+        self.seads_cells = None
 
     def flip(self):
         self.a, self.c = self.c, self.a
@@ -36,10 +38,68 @@ class Face:
                           (self.a.y + self.b.y + self.c.y) / 3,
                           (self.a.z + self.b.z + self.c.z) / 3))
 
+    def get_vertices(self):
+        return [v.pos for v in self.vxs]
+
+    def bbox(self):
+        verts = np.asarray(self.get_vertices())
+        x_min, x_max = np.min(verts[:, 0]), np.max(verts[:, 0])
+        y_min, y_max = np.min(verts[:, 1]), np.max(verts[:, 1])
+        z_min, z_max = np.min(verts[:, 2]), np.max(verts[:, 2])
+
+        return np.asarray((x_min, y_min, z_min)), np.asarray((x_max, y_max, z_max))
+
 
 class Mesh:
     def __init__(self):
         self.faces = set()
+
+    def bbox(self):
+        all_verts = np.concatenate([f.get_vertices() for f in self.faces])
+        x_min, x_max = np.min(all_verts[:, 0]), np.max(all_verts[:, 0])
+        y_min, y_max = np.min(all_verts[:, 1]), np.max(all_verts[:, 1])
+        z_min, z_max = np.min(all_verts[:, 2]), np.max(all_verts[:, 2])
+
+        return np.array((x_min, y_min, z_min)), np.array((x_max, y_max, z_max))
+
+    def construct_seads(self, shape):
+        bbox = self.bbox()
+        bbox_origin = bbox[0]
+
+        grid = np.zeros(shape, dtype=object)
+        grid = np.expand_dims(grid, axis=3)
+        cell_size = (bbox[1] - bbox[0]) / shape
+
+        for f in self.faces:
+            tri_bbox = np.floor((f.bbox() - bbox_origin)/cell_size).astype(int)
+
+    def conform_normals(self):
+        processed = set()
+        self.construct_seads(np.array((100, 100, 100)))
+
+        for f in self.faces:
+            if f not in processed:
+                # shoot a ray at the centroid of the face
+                centroid = f.centroid()
+                ray_origin = random_points_on_sphere(r=250, n=1)[0] + centroid
+                ray = (centroid - ray_origin) * 1000
+                intersections = []
+
+                for g in self.faces:
+                    inter = ray_intersect(ray, ray_origin, g, epsilon=0.0000001)
+
+                    if inter is not None:
+                        dist = np.linalg.norm(inter - ray_origin)
+                        intersections.append((dist, g))
+                        processed.add(g)
+
+                for i in enumerate(sorted(intersections, key=lambda x: x[0])):
+                    if i[0] % 2 == 0:
+                        if np.dot(ray, i[1][1].normal()) > 0:
+                            i[1][1].flip()
+                    else:
+                        if np.dot(ray, i[1][1].normal()) < 0:
+                            i[1][1].flip()
 
 
 def parse_obj(fn):
@@ -88,11 +148,9 @@ def random_points_on_sphere(r, n):
 
 def ray_intersect(ray, ray_origin, tri, epsilon):
     edge1 = tri.b.pos - tri.a.pos
-
     edge2 = tri.c.pos - tri.a.pos
 
     h = np.cross(ray, edge2)
-
     a = np.dot(edge1, h)
 
     if -epsilon < a < epsilon:
@@ -119,10 +177,6 @@ def ray_intersect(ray, ray_origin, tri, epsilon):
         return None
 
 
-def align_normals():
-    pass
-
-
 def write_obj():
     pass
 
@@ -145,24 +199,5 @@ if __name__ == '__main__':
     joined_meshes = group_triangles(faces)
     print(f'Joined meshes in {time.perf_counter() - start_time:.3f}s')
 
-    for i in joined_meshes:
-
-        processed = set()
-
-        for f in i.faces:
-            if f not in processed:
-                # shoot a ray at the centroid of the face
-                centroid = f.centroid()
-                ray_origin = random_points_on_sphere(r=250, n=1)[0] + centroid
-                ray = centroid - ray_origin
-
-                intersections = []
-                for g in i.faces:
-                    int = ray_intersect(ray, ray_origin, g, epsilon=0.0000001)
-
-                    if int is not None:
-                        processed.add(g)
-                        dist = np.linalg.norm(int - ray_origin)
-                        intersections.append((dist, g))
-
-                ordered_intersections = sorted(intersections, key=lambda x: x[0])
+    for m in joined_meshes:
+        m.conform_normals()
